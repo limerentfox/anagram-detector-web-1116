6efe8d7ace113fda684a143ea68114022c9650cb
limerentfox <christi@limerentfox.com>
2016-12-04 00:35:06 -0500

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(possible_matches)
+        matches = []
+        initial_word = @word.chars.to_a
+        possible_matches.each do |word|
+            character_array = word.chars.to_a
+            matches << word if initial_word.sort == character_array.sort
+        end
+        matches
+    end
+end
